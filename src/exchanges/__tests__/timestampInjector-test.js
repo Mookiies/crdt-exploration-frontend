f77ca49708cc5e0c8377c404f0d88607c7bbd9c1
Malcolm Scruggs <malcolm.scruggs@appfolio.com>
2021-11-09 10:29:31 -0700

    Add injectTimestampMethod to recurse over variables as needed

---
+++ src/exchanges/__tests__/timestampInjector-test.js
@@ -0,0 +1,287 @@
+import { injectTimestampVariables } from '../timestampInjector';
+
+describe('injectTimestampVariables', () => {
+
+  const mockTimestamp = 'mockTimestamp'
+
+  it('fills nested fields', () => {
+    const source = {
+      one: {
+        existing: 123,
+        other: null,
+        undi: undefined,
+        array: [{ }, { a: 1}],
+        two: {
+          three: {
+          }
+        },
+        two_two: {
+          three: {  }
+        }
+      }
+    }
+
+    const fieldsToFill = {
+      one: {
+        _timestamped: ['existing', 'other', 'undi'],
+        two: {
+          _timestamped: ['three'],
+          three: {
+          }
+        },
+        two_two: {
+          three: {  }
+        }
+      }
+    }
+
+    const expected = {
+      "one": {
+        "existing": 123,
+        "other": null,
+        array: [{ }, { a: 1}],
+        "timestampsAttributes": {
+          "existing": "mockTimestamp",
+          "other": "mockTimestamp",
+          "undi": "mockTimestamp"
+        },
+        "two": {
+          "three": {},
+          "timestampsAttributes": {
+            "three": "mockTimestamp"
+          }
+        },
+        "two_two": {
+          "three": {}
+        }
+      }
+    }
+
+    const res = injectTimestampVariables(source, fieldsToFill, mockTimestamp);
+    expect(res).toEqual(expected)
+  });
+
+  it('does not add timestamps if they key does not exist on source', () => {
+    const source = {
+      one: {
+        one: 1,
+        two: 2
+      }
+    }
+
+    const fieldsToFill = {
+      one: {
+        _timestamped: ['x', 'y', 'z'],
+        two: {
+          _timestamped: ['one'],
+        },
+      }
+    }
+
+    const expected = {
+      one: {
+        one: 1,
+        two: 2
+      }
+    }
+
+    const res = injectTimestampVariables(source, fieldsToFill, mockTimestamp);
+    expect(res).toEqual(expected)
+  })
+
+  it('returns same object if no fills', () => {
+    const source = {
+      one: {
+        existing: 123,
+        other: null,
+        undi: undefined,
+        two: {
+          three: {}
+        },
+        two_two: {
+          three: {}
+        }
+      }
+    }
+
+
+    const res = injectTimestampVariables(source, { }, mockTimestamp);
+    expect(res).toEqual(source)
+    expect(res).not.toBe(source)
+  })
+
+  it('does not mutate the source object', () => {
+    const source = {
+      arr: [
+        { one: 1 },
+        { one: 1 },
+      ],
+      one: {
+        one: 1,
+        two: 2
+      }
+    }
+
+    const fieldsToFill = {
+      arr: {
+        _timestamped: ['one'],
+      },
+      one: {
+        _timestamped: ['one', 'two'],
+      }
+    }
+
+    const expected = {
+      arr: [
+        { one: 1,
+          timestampsAttributes: {
+            one: mockTimestamp,
+          },
+        },
+        { one: 1,
+          timestampsAttributes: {
+            one: mockTimestamp,
+          },
+        },
+      ],
+      one: {
+        timestampsAttributes: {
+          one: mockTimestamp,
+          two: mockTimestamp
+        },
+        one: 1,
+        two: 2
+      }
+    }
+
+    const res = injectTimestampVariables(source, fieldsToFill, mockTimestamp);
+    expect(res).toEqual(expected)
+    expect(res).not.toBe(source)
+    expect(res).not.toEqual(source)
+  });
+
+  it('does works on nested arrays', () => {
+    const source = {
+      inspectionsInput: {
+        name: 'name',
+        other: 'other',
+        areas: [
+          {
+            name: 'area - 1',
+            other: 'other',
+            items: [
+              {
+                name: 'item - 1',
+                other: 'other',
+              },
+              {
+                name: 'item - 2',
+                other: 'other',
+              }
+            ]
+          },
+          {
+            name: 'area - 2',
+            other: 'other',
+            items: [
+              {
+                name: 'item - 3',
+                other: 'other',
+              },
+              {
+                name: 'item - 4',
+                other: 'other',
+              }
+            ]
+          }
+        ]
+      }
+    }
+
+    const toTimestamp = {
+      inspectionsInput: {
+        _timestamped: ['name', 'other'],
+        areas: {
+          _timestamped: ['name', 'other'],
+          items: {
+            _timestamped: ['name', 'other'],
+          }
+        }
+      }
+    };
+
+    const expected = {
+      inspectionsInput: {
+        name: 'name',
+        other: 'other',
+        timestampsAttributes: {
+          name: mockTimestamp,
+          other: mockTimestamp,
+        },
+        areas: expect.arrayContaining([
+          {
+            name: 'area - 1',
+            other: 'other',
+            timestampsAttributes: {
+              name: mockTimestamp,
+              other: mockTimestamp,
+            },
+            items: expect.arrayContaining([
+              {
+                name: 'item - 1',
+                other: 'other',
+                timestampsAttributes: {
+                  name: mockTimestamp,
+                  other: mockTimestamp,
+                },
+              },
+              {
+                name: 'item - 2',
+                other: 'other',
+                timestampsAttributes: {
+                  name: mockTimestamp,
+                  other: mockTimestamp,
+                },
+              }
+            ])
+          },
+          {
+            name: 'area - 2',
+            other: 'other',
+            timestampsAttributes: {
+              name: mockTimestamp,
+              other: mockTimestamp,
+            },
+            items: expect.arrayContaining([
+              {
+                name: 'item - 3',
+                other: 'other',
+                timestampsAttributes: {
+                  name: mockTimestamp,
+                  other: mockTimestamp,
+                },
+              },
+              {
+                name: 'item - 4',
+                other: 'other',
+                timestampsAttributes: {
+                  name: mockTimestamp,
+                  other: mockTimestamp,
+                },
+              }
+            ])
+          }
+        ])
+      }
+    }
+
+    const res = injectTimestampVariables(source, toTimestamp, mockTimestamp);
+    expect(res).toEqual(expected)
+  });
+
+  // More test cases
+  // empty object
+  // undefined top level
+  // just top level stuff
+  // super nested???
+});
--- src/exchanges/timestampInjector.ts
+++ src/exchanges/timestampInjector.ts
@@ -2,10 +2,12 @@ import type { Exchange, Operation } from 'urql';
 
 import { filter, merge, pipe, tap, map, share } from 'wonka';
 import { makeOperation } from '@urql/core';
+import { isEmpty, cloneDeep } from 'lodash';
 import type HybridLogicalClock from '../lib/hybridLogicalClock';
 
-export type TimestampInjectorExchange = {
+export type TimestampInjectorExchangeOpts = {
   localHlc: HybridLogicalClock;
+  fillConfig?: TimestampsConfig;
 };
 //
 // let timestampedFields = {
@@ -40,7 +42,66 @@ export type TimestampInjectorExchange = {
 //
 // }
 
-export const timestampInjectorExchange = (options: TimestampInjectorExchange): Exchange => ({
+interface TimestampBase {
+  [key: string]: TimestampsConfig;
+}
+
+type TimestampsObj = TimestampBase & {
+  _timestamped?: string[]
+}
+
+type TimestampsConfig = TimestampsObj | undefined;
+
+export const generateTimestamps = (source: any, values: TimestampsConfig, timestamp: string) => {
+  if (!values) {
+    return {};
+  }
+
+  const timestampsToFill = values._timestamped
+  if (!timestampsToFill) {
+    return {};
+  }
+
+  const results: { [key: string]: string } = {};
+  timestampsToFill.forEach(key => {
+    key in source && (results[key] = timestamp)
+  })
+
+  return isEmpty(results) ? {} : { timestampsAttributes: { ...results } };
+}
+
+export const fillMeIn = (source: any, values: TimestampsConfig, timestamp: string) => {
+  if (Array.isArray(source)) {
+    source.forEach(value => {
+      fillMeIn(value, values, timestamp);
+    })
+  }
+
+  if (typeof source !== 'object' || source === null) {
+    return source;
+  }
+
+  const levelResult = source;
+
+  const filledResults = generateTimestamps(source, values, timestamp);
+  Object.assign(levelResult, filledResults);
+
+  const keys = Object.keys(source);
+  keys.forEach(key => {
+    const nextSource = source[key];
+    const nextValues = values && values[key];
+    Object.assign(levelResult, { [key]: fillMeIn(nextSource, nextValues, timestamp) })
+  })
+
+  return levelResult;
+}
+
+export const injectTimestampVariables = (source: any, values: TimestampsConfig, timestamp: string) => {
+  const sourceCopy = cloneDeep(source);
+  return fillMeIn(sourceCopy, values, timestamp);
+}
+
+export const timestampInjectorExchange = (options: TimestampInjectorExchangeOpts): Exchange => ({
                                                                                        forward,
                                                                                        client,
                                                                                        dispatchDebug,
