725e4264b433d2d65d8bdd303b92397e8c5de6ed
Malcolm Scruggs <malcolm.scruggs@appfolio.com>
2021-11-29 16:25:37 -0600

    Add two tests for custom offlineExchange behavior

--- src/exchanges/__tests__/offlineExchange.test.ts
+++ src/exchanges/__tests__/offlineExchange.test.ts
@@ -9,10 +9,20 @@ import {
 
 import { pipe, map, makeSubject, tap, publish } from 'wonka';
 import { offlineExchange } from '../graphcache/src';
+import {GraphQLError} from 'graphql';
 
 const mutationOne = gql`
   mutation {
     updateAuthor {
+      id
+      name
+    }
+  }
+`;
+
+const mutationTwo = gql` 
+  mutation ($name: string) {
+    updateAuthor (name: $name) {
       id
       name
     }
@@ -340,17 +350,216 @@ describe('offline', () => {
     );
   });
 
+  it('should retry other optimistic mutations when an un-retryable error occurs', () => {
+    const onlineSpy = jest.spyOn(navigator, 'onLine', 'get');
 
+    const client = createClient({ url: 'http://0.0.0.0' });
+    const queryOp = client.createRequestOperation('query', {
+      key: 1,
+      query: queryOne,
+    });
+
+    const reexecuteOperation = jest
+      .spyOn(client, 'reexecuteOperation')
+      .mockImplementation(() => undefined);
+
+    const validMutationOp = client.createRequestOperation('mutation', {
+      key: 2,
+      query: mutationTwo,
+      variables: {
+        name: 'name vars'
+      },
+    });
+
+    const failMutationOp = client.createRequestOperation('mutation', {
+      key: 3,
+      query: mutationOne,
+      variables: {},
+    });
+
+    const response = jest.fn(
+      (forwardOp: Operation): OperationResult => {
+        if (forwardOp.key === queryOp.key) {
+          onlineSpy.mockReturnValueOnce(true);
+          return { operation: forwardOp, data: queryOneData };
+        } else if (forwardOp.key === failMutationOp.key) {
+          onlineSpy.mockReturnValueOnce(true);
+          return {
+            operation: forwardOp,
+            error: {
+              name: 'error name',
+              message: 'error message',
+              graphQLErrors: [new GraphQLError('big bad error')]
+            }
+          }
+        } else {
+          onlineSpy.mockReturnValueOnce(false);
+          return {
+            operation: forwardOp,
+            // @ts-ignore
+            error: { networkError: new Error('failed to fetch') },
+          };
+        }
+      }
+    );
+
+    const { source: ops$, next } = makeSubject<Operation>();
+    const result = jest.fn();
+    const forward: ExchangeIO = ops$ => pipe(ops$, map(response));
+
+    storage.readData.mockReturnValueOnce({ then: () => undefined });
+    storage.readMetadata.mockReturnValueOnce({ then: () => undefined });
+    storage.writeMetadata.mockReturnValue({ then: () => undefined });
+
+    pipe(
+      offlineExchange({
+        storage,
+        persistedContext: [],
+        isRetryableError: (res) => res.operation.key !== failMutationOp.key,
+        optimistic: {
+          updateAuthor: () => ({
+            id: '123',
+            name: 'URQL',
+            __typename: 'Author',
+          }),
+        },
+      })({ forward, client, dispatchDebug })(ops$),
+      tap(result),
+      publish
+    );
+
+    next(validMutationOp);
+    expect(result).toBeCalledTimes(0);
+    expect(storage.writeMetadata).toBeCalledTimes(1);
+    expect(storage.writeMetadata).toHaveBeenCalledWith([
+      {
+        query: `mutation ($name: string) {
+  updateAuthor(name: $name) {
+    id
+    name
+    __typename
+  }
+}`,
+        variables: {
+          name: 'name vars'
+        },
+        context: {}
+      },
+    ]);
+
+    next(failMutationOp);
+    expect(result).toBeCalledTimes(2);
+    expect(result.mock.calls[0][0].operation).toMatchObject({
+      kind: 'teardown',
+      key: validMutationOp.key
+    })
+    expect(result.mock.calls[1][0]).toMatchObject({
+      operation: {
+        key: failMutationOp.key,
+        kind: 'mutation',
+      },
+      error: {
+        name: 'error name',
+        graphQLErrors: expect.any(Array),
+      },
+    })
+    expect(reexecuteOperation).toHaveBeenCalledTimes(1);
+    expect((reexecuteOperation.mock.calls[0][0] as any).key).toEqual(validMutationOp.key);
+    expect((reexecuteOperation.mock.calls[0][0] as any).variables).toEqual(validMutationOp.variables);
+    expect((reexecuteOperation.mock.calls[0][0] as any).query).toEqual(
+      formatDocument(validMutationOp.query)
+    );
+    expect(storage.writeMetadata).toBeCalledTimes(3);
+    // TODO how could I set this up so that the optimistic result is still the same after rexecuteOperation
+    // would need to have some way of getting rexecuteOperation to actually do something :(
+    // currently this last write metadata call is just to be used as the clear of the flush queue
+
+    next(queryOp);
+    expect(result).toBeCalledTimes(3);
+    expect(result.mock.calls[2][0].data).toMatchObject(queryOneData);
+  });
+
+  it('does not re-execute mutations that have successfully returned', () => {
+    let flush: () => {};
+    storage.onOnline.mockImplementation(cb => {
+      flush = cb;
+    });
+
+    const client = createClient({ url: 'http://0.0.0.0' });
+    const reexecuteOperation = jest
+      .spyOn(client, 'reexecuteOperation')
+      .mockImplementation(() => undefined);
+
+    const mutationOp = client.createRequestOperation('mutation', {
+      key: 1,
+      query: mutationOne,
+      variables: {},
+    });
+
+    const response = jest.fn(
+      (forwardOp: Operation): OperationResult => {
+        return { operation: forwardOp, data: mutationOneData };
+      }
+    );
+
+    const { source: ops$, next } = makeSubject<Operation>();
+    const result = jest.fn();
+    const forward: ExchangeIO = ops$ => pipe(ops$, map(response));
+
+    storage.readData.mockReturnValueOnce({ then: () => undefined });
+    storage.readMetadata.mockReturnValueOnce({ then: () => undefined });
+    storage.writeMetadata.mockReturnValueOnce({ then: () => undefined });
+
+    pipe(
+      offlineExchange({
+        storage,
+        persistedContext: [],
+        isRetryableError: () => true,
+        optimistic: {
+          updateAuthor: () => ({
+            id: '123',
+            name: 'URQL',
+            __typename: 'Author',
+          }),
+        },
+      })({ forward, client, dispatchDebug })(ops$),
+      tap(result),
+      publish
+    );
+
+    next(mutationOp);
+    expect(storage.writeMetadata).toBeCalledTimes(2);
+    expect(storage.writeMetadata).toHaveBeenCalledWith([
+      {
+        query: `mutation {
+  updateAuthor {
+    id
+    name
+    __typename
+  }
+}`,
+        variables: {},
+        context: {},
+      },
+    ]);
+    expect(storage.writeMetadata).toHaveBeenCalledWith([]);
+
+    flush!();
+    expect(reexecuteOperation).toHaveBeenCalledTimes(0);
+  })
 });
 
 // persisted context test
 // [x] only chooses keys that come in config
 
 // Change to inflightOperations (can probably combine some of these)
-// [] updating metadata when a mutation hasn't failed yet
-// [] deleted from inflight when a valid mutaion comes back
-// [] deleted when it's an unretryable error
-// [] persisted if it's an error that can be retried
+// [x] updating metadata when a mutation hasn't failed yet (how does this work with next being sync and all that jazz)
+// [x] deleted from inflight when a valid mutaion comes back
+// [kinda] deleted when it's an unretryable error - (kinda b/c only loosely tested because re-execute doesn't do anything
+// [x] persisted if it's an error that can be retried
 
 // Retries
-// []
+// [x] - basic case for a queued up mutation and a retry
+// [kinda] - multiple stacked mutations (kinda b/c one test with a fail)
+// [] - returned optimistic results from mutations
+// [] -
