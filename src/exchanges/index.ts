8aae43b5c98701e3ba70b7d42a1903cbdf3b90df
Daniel Vicory <dvicory@gmail.com>
2022-02-03 20:10:19 -0800

    Introduce new CRDT exchange

--- src/exchanges/index.ts
+++ src/exchanges/index.ts
@@ -1,2 +1,3 @@
+export { crdtExchange } from './crdtExchange';
 export { timestampExchange, PROCESSED_OPERATION_KEY as TIMESTAMPS_PROCESSED_OPERATION_KEY } from './timestampExchange';
 export { patchExchange, PROCESSED_OPERATION_KEY as PATCH_PROCESSED_OPERATION_KEY } from './patchExchange';
